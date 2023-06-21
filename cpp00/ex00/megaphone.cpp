94a0418400121bcaf2a259c550cd36d0b9331148
Zakaria Sabri <zasabri@e2r9p6.1337.ma>
2023-06-21 18:28:48 +0100

    @5tirner

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/21 17:29:37 by zasabri           #+#    #+#             */
+/*   Updated: 2023/06/21 18:09:31 by zasabri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class MegaPhone
+{
+	public:
+		void	ToUpper(char *str)
+		{
+			int	i = 0;
+			while (str[i])
+			{
+				if (str[i] >= 'a' && str[i] <= 'z')
+					str[i] -= 32;
+				std::cout<<str[i];
+				i++;
+			}
+		}
+};
+
+int main(int ac, char** av)
+{
+	int	i = 1;
+	MegaPhone MyObg;
+	while (i < ac)
+	{
+		MyObg.ToUpper(av[i]);
+		i++;	
+	}
+	if (i == 1)
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout<<"\n";
+}
