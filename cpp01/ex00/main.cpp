91b01032bffe8cc1d84e19f1b523ce8546e973f0
Zakaria Sabri <zasabri@e1r3p8.1337.ma>
2023-07-04 18:21:56 +0100

    @5tirner

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -1,20 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/25 16:14:46 by zasabri           #+#    #+#             */
-/*   Updated: 2023/07/04 18:21:08 by zasabri          ###   ########.fr       */
+/*   Created: 2023/07/04 18:20:35 by zasabri           #+#    #+#             */
+/*   Updated: 2023/07/04 18:21:17 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-void	Zombie::announce(void)
-{	
-	std::cout << name
-		<< ": BraiiiiiiinnnzzzZ..."
-		<< std::endl;
+int main()
+{
+    Zombie	*TWD;
+	
+	TWD = newZombie("Foo");
+	TWD->announce();
+	randomChump("RICK GRAMES");
+	delete TWD;
 }
