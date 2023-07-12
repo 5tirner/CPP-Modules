acfde768b93d03087a0cb8bed4052d848d4fdeed
Zakaria Sabri <zasabri@e1r6p4.1337.ma>
2023-07-12 08:53:31 +0100

    @5tirner

--- func_ptr.c
+++ func_ptr.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 
-// void print(int a)
-// {
-//     a += 32;
-//     printf("%c\n", a);
-// }
-
-// int main()
-// {
-//     void (*try)(int);
-//     try = &print;
-
-//     (*try)('A');    
-// }
-
-int main(int ac, char **av)
+void print(int a)
 {
-    int i = 0;
-    while (av[i])
-        i++;
-    printf("%d\n", i);
+    a += 32;
+    printf("%c\n", a);
+}
+
+int main()
+{
+    void (*try)(int);
+    try = &print;
+
+    (*try)('A');    
 }
---
+++ membrefuncPtr.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class name
+{
+    public:
+        int get1()
+        {
+            return (1);
+        }
+        int get2()
+        {
+            return (2);
+        }
+};
+
+int p(name *obj, int (name::*r)(void))
+{
+    return ((obj->*r)());
+}
+
+int t(name *obj, int (name::*y)(void))
+{
+    int t = (obj->*y)();
+    return (t);
+}
+int main()
+{
+    name ob;
+    std::cout << "check get one: " << p(&ob, &name::get1) << std::endl;
+    std::cout << "check get two: " << t(&ob, &name::get2) << std::endl; 
+    return (0);
+}
