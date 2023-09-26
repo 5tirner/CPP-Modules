7ac009b51bcf98ee0c9382ef526d69dbc60e9520
Zakaria Sabri <zasabri@c4r1p1.1337.ma>
2023-09-26 16:00:57 +0100

    zasabri

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,20 +6,22 @@
 /*   By: zasabri <zasabri@student.1337>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 17:02:03 by zasabri           #+#    #+#             */
-/*   Updated: 2023/09/26 15:34:37 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/09/26 15:55:53 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 int	main()
 {
 	std::cout << "-------------------------" << '\n';
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
+	const Animal* meta = new Animal;
+	const Animal* j = new Dog;
+	const Animal* i = new Cat;
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
 	i->makeSound(); //will output the cat sound!
@@ -28,5 +30,9 @@ int	main()
 	delete i;
 	delete j;
 	delete meta;
+	std::cout << "------------------" << '\n';
+	WrongAnimal	*wa = new WrongCat;
+	wa->makeSound();
+	delete wa;
 	return (0);
 }
