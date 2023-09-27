bf463fb12d14c3ca0d15e7c328afda7a0706fe75
Zakaria Sabri <zasabri@c3r6p4.1337.ma>
2023-09-27 14:14:06 +0100

    zasabri

---
+++ cpp04/ex03/ICharacter.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zasabri <zasabri@student.1337>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 16:55:45 by zasabri           #+#    #+#             */
+/*   Updated: 2023/09/26 17:06:42 by zasabri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ICHARACTER_HPP
+#define ICHARACTER_HPP
+
+#include "AMateria.hpp"
+
+class ICharacter
+{
+    public:
+        ICharacter(void);
+        ICharacter(std::string param);
+        ICharacter(const ICharacter &other);
+        ICharacter&operator=(const ICharacter &other);
+        virtual ~ICharacter(void);
+        virtual std::string const & getName() const = 0;
+        virtual void equip(AMateria* m) = 0;
+        virtual void unequip(int idx) = 0;
+        virtual void use(int idx, ICharacter& target) = 0;
+};
+
+#endif
