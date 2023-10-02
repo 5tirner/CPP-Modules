b0da62eab1c6d856c4052e08f95b18f3df3f5d89
5tirner <thatfor1337@gmail.com>
2023-10-02 04:52:36 +0100

    zasabri

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zasabri <zasabri@student.1337>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 17:02:03 by zasabri           #+#    #+#             */
-/*   Updated: 2023/09/21 19:38:02 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/09/28 13:44:55 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,7 @@
 
 int	main()
 {
-	//Animal a;
 	Animal *a;
 	a = NULL;
-	Animal *b;
-	Cat e;
-	b = &e;
+	//Animal b;
 }
--- cpp04/ex03/MateriaSource.cpp
+++ cpp04/ex03/MateriaSource.cpp
@@ -1,12 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   IMateriaSource.cpp                                 :+:      :+:    :+:   */
+/*   MateriaSource.cpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zasabri <zasabri@student.1337>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/28 14:27:24 by zasabri           #+#    #+#             */
-/*   Updated: 2023/09/28 14:27:33 by zasabri          ###   ########.fr       */
+/*   Created: 2023/09/28 15:09:50 by zasabri           #+#    #+#             */
+/*   Updated: 2023/09/28 15:11:01 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource(void)
+{
+    std::cout << "MateriaSource Constructor Called" << '\n';
+}
