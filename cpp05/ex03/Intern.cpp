43a9d2923b0e4e379abf3d03e26cbd000a1d36b8
Zakaria Sabri <zasabri@e2r2p12.1337.ma>
2023-10-14 09:31:16 +0100

    @5tirner

---
+++ cpp05/ex03/Intern.cpp
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 00:59:33 by zasabri           #+#    #+#             */
+/*   Updated: 2023/10/14 09:30:58 by zasabri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Intern.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
+Intern::Intern(void)
+{
+    std::cout << "Intern Constructor Called" << '\n';
+}
+
+Intern::Intern(const Intern &other)
+{
+    std::cout << "Intern Copy One Called" << '\n';
+    *this = other;
+}
+
+Intern&Intern::operator=(const Intern &other)
+{
+    std::cout << "Intern Assignement One Called" << '\n';
+    (Intern)other;
+    return (*this);
+}
+
+Intern::~Intern(void)
+{
+    std::cout << "Intern Distructor Called" << '\n';
+}
+
+/*Make Form Func*/
+
+std::string goodName(std::string name)
+{
+    int i = 0;
+    std::string r;
+    while (name[i])
+    {
+        if (name[i] != ' ')
+            r += tolower(name[i]);
+        i++;
+    }
+    return (r);
+}
+AForm    *Intern::makeForm(std::string formName, std::string target)
+{
+    AForm       *r;
+    std::string Fname[3];
+    std::string newName = goodName(formName);
+    int         i = 0;
+
+    Fname[0] = "shrubberycreation";
+    Fname[1] = "robotomyrequest";
+    Fname[2] = "presidentialpardon";
+    while (i < 3 && Fname[i] != newName)
+        i++;
+    switch (i)
+    {
+        case 0:
+            std::cout << "Intern Creates A ShrubberyCreationForm" << '\n'; 
+            r = new ShrubberyCreationForm;
+            break;
+        case 1:
+            std::cout << "Intern Creates A RobotomyRequestForm" << '\n';
+            r = new RobotomyRequestForm;
+            break;
+        case 2:
+            std::cout << "Intern Creates A PresidentialPardonForm" << '\n';
+            r = new PresidentialPardonForm;
+            break;
+        default:
+            std::cout << "Form Doesn't Found" << '\n';
+            r = NULL;
+            break;
+    }
+    return (r);
+}
---
+++ cpp05/ex03/Intern.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 00:59:36 by zasabri           #+#    #+#             */
+/*   Updated: 2023/10/14 08:40:28 by zasabri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INTERN_HPP
+# define INTERN_HPP
+
+
+# include <iostream>
+# include "Bureaucrat.hpp"
+# include "AForm.hpp"
+
+class   Intern
+{
+    public:
+        Intern(void);
+        Intern(const Intern &other);
+        Intern&operator=(const Intern &other);
+        ~Intern(void);
+        AForm   *makeForm(std::string FormName, std::string target);
+};
+
+std::string goodName(std::string name);
+
+#endif
