120ccd00337895c3f84037a861ecb39b2a41770a
Zakaria Sabri <zasabri@e2r3p10.1337.ma>
2023-10-17 23:16:50 +0100

    @5tirner

--- cpp06/ex01/Serialization.cpp
+++ cpp06/ex01/Serialization.cpp
@@ -6,7 +6,20 @@
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 18:45:53 by zasabri           #+#    #+#             */
-/*   Updated: 2023/10/17 18:45:54 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/10/17 23:16:46 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Serialization.hpp"
+
+Serializer::Serializer(void) {}
+Serializer::Serializer(const Serializer &other)
+{
+    std::cout << &other << '\n';
+}
+Serializer&Serializer::operator=(const Serializer &other)
+{
+    std::cout << &other << '\n';
+    return (*this);
+}
+Serializer::~Serializer(void) {}
