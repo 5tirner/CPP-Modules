9bdc78a0ddf8645285b874d15ce6fc9f68cd7984
Zakaria Sabri <zasabri@e2r3p10.1337.ma>
2023-10-17 23:14:52 +0100

    @5tirner

--- cpp06/ex01/Serialization.hpp
+++ cpp06/ex01/Serialization.hpp
@@ -6,12 +6,22 @@
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 18:45:56 by zasabri           #+#    #+#             */
-/*   Updated: 2023/10/17 23:05:39 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/10/17 23:14:06 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERIALIZATION_HPP
 # define SERIALIZATION_HPP
+
+#include <iostream>
+
+typedef struct  S_Data
+{
+    char        *s1;
+    std::string *s2;
+    int         d;
+    float       f;
+}   Data;
 
 class   Serializer
 {
@@ -20,6 +30,9 @@ class   Serializer
         Serializer(const Serializer &other);
         Serializer&operator=(const Serializer &other);
         ~Serializer(void);
+    public:
+        static uintptr_t    serialize(Data *ptr);
+        static Data         *deserialize(uintptr_t raw);
 };
 
 #endif
