9ea3103462c202756a58207affc7011a3431f2de
Zakaria Sabri <zasabri@e2r5p11.1337.ma>
2023-10-20 01:55:34 +0100

    @5tirner

--- cpp06/ex02/identify2.cpp
+++ cpp06/ex02/identify2.cpp
@@ -1,28 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   generate.cpp                                       :+:      :+:    :+:   */
+/*   identify2.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/19 02:08:53 by zasabri           #+#    #+#             */
-/*   Updated: 2023/10/19 04:49:51 by zasabri          ###   ########.fr       */
+/*   Created: 2023/10/19 05:02:34 by zasabri           #+#    #+#             */
+/*   Updated: 2023/10/19 19:13:29 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
 
-Base    *generate(void)
+void    identify(Base &p)
 {
-    std::cout << std::rand() << '\n';
-    Base            address;
-    uintptr_t       save = reinterpret_cast<uintptr_t>(&address);
-    unsigned int    a = static_cast<unsigned int>(save);
-
-    a %= 10;
-    if (a < 3)
-        return (new A);
-    if (a < 6)
-        return (new B);
-    return (new C);
 }
