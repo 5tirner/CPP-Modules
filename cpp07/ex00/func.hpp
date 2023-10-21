327c2b37767a65484f52a2bb636e576272fdd13a
Zakaria Sabri <zasabri@e2r2p11.1337.ma>
2023-10-22 00:17:23 +0100

    @5tirner

--- cpp07/ex00/func.hpp
+++ cpp07/ex00/func.hpp
@@ -6,20 +6,32 @@
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 02:50:03 by zasabri           #+#    #+#             */
-/*   Updated: 2023/10/21 03:40:16 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/10/22 00:16:11 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FUNC_HPP
 # define FUNC_HPP
 
-template<typename t>void    swap(t &a, t &b)
+template<typename SWAP>void    swap(SWAP &a, SWAP &b)
 {
-    t tmp = a;
+    SWAP tmp = a;
     a = b;
     b = tmp;
 }
 
-//template<typename>
+template<typename MIN>MIN   min(MIN &a, MIN &b)
+{
+    if (a < b)
+        return (a);
+    return (b);
+}
+
+template<typename MAX>MAX   max(MAX &a, MAX &b)
+{
+    if (a > b)
+        return (a);
+    return (b);
+}
 
 #endif
--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 02:45:34 by zasabri           #+#    #+#             */
-/*   Updated: 2023/10/21 03:40:38 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/10/22 00:16:55 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,14 @@ int main( void )
     int a = 2;
     int b = 3;
     swap( a, b );
-    swap(a , b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    // std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-    // std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-    // std::string c = "chaine1";
-    // std::string d = "chaine2";
-    // ::swap(c, d);
-    // std::cout << "c = " << c << ", d = " << d << std::endl;
-    // std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    // std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-    // return (0);
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return (0);
 }
