e86493556547937f9c0e5a238d2b345b6a2bacfa
Zakaria Sabri <zasabri@e2r4p11.1337.ma>
2023-10-23 14:14:07 +0100

    @5tirner

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 04:01:24 by zasabri           #+#    #+#             */
-/*   Updated: 2023/10/22 04:40:05 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/10/23 14:12:47 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,64 @@
 
 # include <iostream>
 
-template<typename T>class Array
+template<class T>
+class Array
 {
     private:
-        T   *elements;
+        T            *elements;
+        unsigned int elements_size;
     public:
         Array(void)
         {
+            std::cout << "Array Constructor Called" << '\n';
             elements = NULL;
         }
         Array(unsigned int n)
         {
-            elements = new (std::nothrow) T[n];
-            if (!elements)
-                throw("Allocation Failed");
+            std::cout << "Array Paramitrize One Called" << '\n';
+            try
+            {
+                elements = new T[n];
+            }
+            catch(std::bad_alloc &ba)
+            {
+                std::cout << ba.what() << '\n';
+            }
+            this->elements_size = n;
         }
-        Array(conts Array &other);
+        Array(const Array &other)
+        {
+            *this = other;
+        }
+        Array&operator=(const Array &other)
+        {
+            std::cout << "Array Assignment Called" << '\n';
+            if (other.elements)
+            {
+                if (!this->elements)
+                {
+                    this->elements = new T;
+                    *(this->elements) = *(other.elements);
+                    this->elements_size = other.elements_size;
+                }
+                else
+                {
+                    delete this->elements;
+                    this->elements = new T;
+                    *(this->elements) = *(other.elements);
+                    this->elements_size = other.elements_size;
+                }
+            }
+            return (*this);
+        }
+        ~Array(void)
+        {
+            std::cout << "Array Destructor" << '\n';
+            if (this->elements)
+            {
+                delete this->elements;
+            }
+        }
 };
+
 #endif
