d2f56e70f055e0c118bdf2242dfcdf3cea5874d8
Zakaria Sabri <zasabri@e2r5p12.1337.ma>
2023-10-29 04:01:39 +0100

    @5tirner

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 23:48:51 by zasabri           #+#    #+#             */
-/*   Updated: 2023/10/29 03:44:53 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/10/29 03:54:54 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,44 @@
 
 int main()
 {
-    std::cout << "Create A Span\n";
+    std::cout << "Main Tests\n";
     Span sp = Span(5);
-    std::cout << "Add Numbers\n";
-    try
+    sp.addNumber(6);
+    sp.addNumber(3);
+    sp.addNumber(17);
+    sp.addNumber(9);
+    sp.addNumber(11);
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
+    std::cout << "\n---------------------------------" << '\n';
+    std::cout << "My Test\n";
+    std::cout << "1: Create A Span\n";
+    Span test = Span(10000);
+    std::cout << "2: Add Numbers\n";
+    for (int i = 0; i < 10000; i++)
     {
-        sp.addNumber(6);
-        sp.addNumber(3);
-        sp.addNumber(17);
-        sp.addNumber(9);
-        sp.addNumber(11);
+        try
+        {
+            test.addNumber(i);
+        }
+        catch(const char *err)
+        {
+            std::cerr << err << '\n';
+        }
     }
-    catch(const char *err)
-    {
-        std::cerr << err << '\n';
-    }
-    
-    std::cout << "Print1\n";
-    for (unsigned long i = 0; i < 5; i++)
-    {
-        std::cout << "Element" << i << ": " << sp[i] << '\n';
-    }
-    std::cout << "Print Done.\n";
-    std::cout << "Try To find Spans\n";
+    // std::cout << "3: Print\n";
+    // for (unsigned long i = 0; i < 10000; i++)
+    // {
+    //     std::cout << "Element" << i << ": " << test[i] << '\n';
+    // }
+    // std::cout << "4: Print Done.\n";
+    std::cout << "5: Try To find Spans\n";
     try
     {
         std::cout << "Shortest Span: ";
-        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << test.shortestSpan() << std::endl;
         std::cout << "Longest Span: ";
-        std::cout << sp.longestSpan() << std::endl;
+        std::cout << test.longestSpan() << std::endl;
     }
     catch(const char *err)
     {
